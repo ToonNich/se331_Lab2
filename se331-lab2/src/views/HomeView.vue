3602183969bec1e1ec2a7b4915fcc3ec45c426f1
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-07-07 23:09:17 +0700

    7

--- se331-lab2/src/views/HomeView.vue
+++ se331-lab2/src/views/HomeView.vue
@@ -1,4 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EventCard from '@/components/EventCard.vue';
+</script>
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <EventCard />
+  </div>
 </template>
