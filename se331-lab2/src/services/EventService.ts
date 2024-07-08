fc96b3e00cfc031e3051c4b9ed3c82b930e68022
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-07-08 11:29:35 +0700

    16.1

---
+++ se331-lab2/src/services/EventService.ts
@@ -0,0 +1,16 @@
+import axios from "axios"
+
+const apiClient = axios.create({
+    baseURL: 'https://my-json-server.typicode.com/ToonNich/se331_Lab2/events',
+    withCredentials: false,
+    headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+    }
+})
+
+export default {
+    getEvent() {
+        return apiClient.get('/events')
+    }
+}
