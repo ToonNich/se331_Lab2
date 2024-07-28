a09af86e32d9441d3cc046bd167d45e43151d82c
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-07-28 14:44:46 +0700

    1.7

--- se331-lab2/src/components/EventCard.vue
+++ se331-lab2/src/components/EventCard.vue
@@ -20,7 +20,7 @@ defineProps<{
 </script>
 
 <template>
-  <RouterLink to = "/event/5928101">
+  <RouterLink :to = "{ name: 'event-detail-view', params: { id: event.id} }">
     <div class="event-card">
       <h2>{{ event.title }}</h2>
       <span>@{{ event.time }} on {{ event.date }}</span>
