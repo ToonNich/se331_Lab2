d9f0f8bc200b19a66728dc6fd21e7708bcdbd310
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-07-28 16:52:39 +0700

    2.9

--- se331-lab2/src/views/EventListView.vue
+++ se331-lab2/src/views/EventListView.vue
@@ -82,20 +82,26 @@ watchEffect(() => {
     <!-- new element-->
     <div class="events">
       <EventCard v-for="event in events" :key="event.id" :event="event" />
+      <div class="pagination">
+        <RouterLink 
+        id="page-prev"
+        :to="{ name: 'event-list-view', query: { page:page -1 } }"
+        rel="prev"
+        v-if="page != 1">
+        &#60;
+        Prev Page
+        </RouterLink>
+
+        <RouterLink 
+        id="page-next"
+        :to="{ name: 'event-list-view', query: { page:page +1 } }"
+        rel="next"
+        v-if="hasNextPage">
+        Next Page
+        &#62;
+      </RouterLink>
+      </div>
     </div>
-    <RouterLink 
-    :to="{ name: 'event-list-view', query: { page:page -1 } }"
-    rel="prev"
-    v-if="page != 1">
-    Prev Page
-    </RouterLink>
-    <RouterLink 
-    :to="{ name: 'event-list-view', query: { page:page +1 } }"
-    rel="next"
-    v-if="hasNextPage">
-    Next Page
-    </RouterLink>
-    
 </template>
 
 <style scoped>
@@ -103,5 +109,20 @@ watchEffect(() => {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
