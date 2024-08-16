5bdfbbc47710094a9890bc9093c534bed5a0fac9
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-08-16 20:52:11 +0700

    1.4

--- se331-lab2/src/views/event/RegisterView.vue
+++ se331-lab2/src/views/event/RegisterView.vue
@@ -2,6 +2,7 @@
 import { toRefs, defineProps } from 'vue'
 import { type Event } from '@/types'
 import { useRouter } from 'vue-router'
+import { useMessageStore } from '@/stores/message'
 
 const props = defineProps<{
   event: Event
@@ -12,11 +13,16 @@ const { event } = toRefs(props)
 
 const router = useRouter()
 
+const store = useMessageStore()
+
 const register = () => {
-  // If...
-  // Push...
-  router.push({ name: 'event-detail-view' })
+  store.updateMessage('You are successfully registered for ' + props.event.title)
+setTimeout(() => {
+  store.resetMessage()
+}, 3000)
+router.push({ name: 'event-detail-view' , params: { id: props.event.id} })
 }
+
 </script>
 <template>
   <p>Register event here</p>
