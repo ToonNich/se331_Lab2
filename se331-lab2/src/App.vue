63408affa4eb4426303ba802b9a0c69b10e851c1
Nichapat <Nichapat_Padung@cmu.ac.th>
2024-09-13 23:10:59 +0700

    7.6

--- se331-lab2/src/App.vue
+++ se331-lab2/src/App.vue
@@ -37,6 +37,14 @@ const { message } = storeToRefs(store)
            :to="{ name: 'student' }"
 >Student</RouterLink
 >
+|
+<RouterLink
+           class="font-bold text-gray-700"
+           exact-active-class="text-green-500"
+           :to="{ name: 'add-event' }"
+>New Event</RouterLink
+>
+
 </nav>
 </div>
 </header>
